94c83de4f8c865920c8b2e0dcc39a74cb1743636
Vladimir <sokolov_elista@mail.ru>
2022-10-24 22:44:26 +0300

    First "Spring MVC + Hibernate + AOP" project commit.

---
+++ src/main/java/spring_mvc_hibernate_aop/controller/MyController.java
@@ -0,0 +1,45 @@
+package spring_mvc_hibernate_aop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring_mvc_hibernate_aop.dao.EmployeeDAO;
+import spring_mvc_hibernate_aop.entity.Employee;
+import spring_mvc_hibernate_aop.service.EmployeeService;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+//    private EmployeeDAO employeeDAO; // строчка кода старой архитектуры уже не нужна и заменена на ниже идущую.
+    private EmployeeService employeeService;
+
+    @RequestMapping("/") //будем возвращать список работников, когда в адресной строке браузера укажем слэш.
+    public String showAllEmployees (Model model ) {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-employees";
+    }
+
+
+    @RequestMapping("/addNewEmployee")
+    public String addNewEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+
+        return "employee-info";
+    }
+
+    @RequestMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return "redirect:/";
+    }
+
+
+}
---
+++ src/main/java/spring_mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package spring_mvc_hibernate_aop.dao;
+
+import spring_mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+}
